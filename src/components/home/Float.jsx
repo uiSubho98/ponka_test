634e9b1f665529b9a025056557fbab1fc99fdb60
unknown <dsubhojit063@gmail.com>
2024-06-19 04:01:29 +0530

    footer added

--- src/components/home/Float.jsx
+++ src/components/home/Float.jsx
@@ -5,7 +5,7 @@ const Float = () => {
   return (
     <NavLink to="/support">
       <img
-        className="fixed bottom-5 float sm:right-10 right-0 cursor-pointer bg-[#009963] text-white p-2 rounded-full w-[80px] h-[80px] shadow-lg z-50 border-4 border-[#009963]"
+        className="fixed bottom-[30%] float sm:right-10 right-2 cursor-pointer bg-[#009963] text-white p-2 rounded-full w-[80px] h-[80px] shadow-lg z-50 border-4 border-[#009963]"
         src={floatImage}
         alt="Support Float Button"
       />
