7ee9bfcf7dbc48f76c11e0df5dd12414de4ddcc0
unknown <dsubhojit063@gmail.com>
2024-06-23 00:27:45 +0530

    about page done

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,14 +1,19 @@
 import play from "../assets/play.png";
 
 const Video = () => {
+  const isIOS =
+    /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+
   return (
     <div className="h-fit md:h-[30rem] flex justify-center items-center overflow-hidden">
-      <div className="relative w-full "style={{ paddingTop: '56.25%', }}>
+      <div className="relative w-full " style={{ paddingTop: "56.25%" }}>
         <video
+          playsInline={isIOS}
+          webkit-playsinline={isIOS ? "true" : undefined}
           src="https://res.cloudinary.com/dbohkz4wr/video/upload/v1719007245/videoplayback_1_online-video-cutter.com_b8zekv.mp4"
           className="absolute top-0 left-0 w-full h-full "
-          autoPlay  // corrected attribute for autoplay
-          muted     // video should be muted for autoplay to work in most browsers
+          autoPlay // corrected attribute for autoplay
+          muted // video should be muted for autoplay to work in most browsers
           loop
           title="Ponka"
         ></video>
